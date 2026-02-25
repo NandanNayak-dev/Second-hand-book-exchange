71cab79c0f0fe8a71d966a0d2572c452d5f0a9b9
Nandan Nayak <nandannayak2102@gmail.com>
2026-02-25 14:35:41 +0530

    Review Author Authorization

--- middleware.js
+++ middleware.js
@@ -1,4 +1,6 @@
 const booklist = require("./models/booklist");
+const Review = require("./models/review");
+
 module.exports.isLoggedIn = (req, res, next) => {
     if (!req.isAuthenticated()) {
         req.session.redirectUrl = req.originalUrl;
@@ -7,12 +9,14 @@ module.exports.isLoggedIn = (req, res, next) => {
     }
     next();
 };
+
 module.exports.saveRedirectUrl = (req, res, next) => {
     if(req.session.redirectUrl){
         res.locals.redirectUrl=req.session.redirectUrl;
     }
     next();
 };
+
 module.exports.isOwner = async (req, res, next) => {
     const { id } = req.params;
     const book = await booklist.findById(id);
@@ -20,5 +24,22 @@ module.exports.isOwner = async (req, res, next) => {
         req.flash("error", "You do not have permission to do that!");
         return res.redirect(`/booklistings/${id}`);
     }
+    next();
+};
+
+module.exports.isReviewAuthor = async (req, res, next) => {
+    const { id, reviewId } = req.params;
+    const review = await Review.findById(reviewId);
+
+    if (!review) {
+        req.flash("error", "Review not found");
+        return res.redirect(`/booklistings/${id}`);
+    }
+
+    if (!review.author || !review.author.equals(req.user._id)) {
+        req.flash("error", "You do not have permission to do that!");
+        return res.redirect(`/booklistings/${id}`);
+    }
+
     next();
 };
--- routes/review.js
+++ routes/review.js
@@ -3,24 +3,28 @@ const router = express.Router({ mergeParams: true });
 const wrapAsync = require("../utils/wrapAsync");
 const Review = require("../models/review");
 const booklist = require("../models/booklist");
+const {isLoggedIn,isReviewAuthor}=require("../middleware");
 
 //=============REVIEWS===========================
-router.post("/",wrapAsync(async(req,res)=>{
+router.post("/",isLoggedIn,wrapAsync(async(req,res)=>{
     const {id}=req.params;
     const book=await booklist.findById(id);
     const review=new Review(req.body.review);
+    review.author = req.user._id;
     book.reviews.push(review._id);
     await review.save();
     await book.save();
     req.flash("success","Review Added Successfully");
     res.redirect(`/booklistings/${book._id}`);
 }))
+
 //====Delete Review=====
-router.delete("/:reviewId",wrapAsync(async(req,res)=>{
+router.delete("/:reviewId",isLoggedIn,isReviewAuthor,wrapAsync(async(req,res)=>{
     const {id,reviewId}=req.params;
     await booklist.findByIdAndUpdate(id,{$pull:{reviews:reviewId}});
     await Review.findByIdAndDelete(reviewId);
     req.flash("success","Review Deleted Successfully");
     res.redirect(`/booklistings/${id}`);
 }))
+
 module.exports=router;
